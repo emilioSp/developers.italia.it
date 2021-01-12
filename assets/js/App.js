88e6a6c8ee2837ce608809568c82356a345155e8
emilioSp <emilio.spatola@gmail.com>
2021-01-12 12:29:34 +0100

    First draft with portals

---
+++ assets/js/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { CatalogueSearch } from './components/CatalogueSearch.js';
+import { CatalogueResults } from './components/CatalogueResults.js';
+
+export const App = () => (
+  <>
+    <CatalogueSearch />
+    <CatalogueResults />
+  </>
+);
--- assets/js/components/CatalogueSearch.js
+++ assets/js/components/CatalogueSearch.js
@@ -1,16 +1,84 @@
-import React from "react";
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
 export const CatalogueSearch = () => {
-  return (
-    <a id="search-form" data-toggle="modal" data-backdrop="false" data-target="#searchModal">
-      <div className="form-inline pr-2 pr-lg-0" role="search">
-        <div className="form-group position-relative">
-          <span className="text-white mr-3 d-none d-lg-inline">CERCA</span>
-          <svg className="icon">
-            <use xlinkHref="/assets/svg/sprite.svg#it-search"></use>
-          </svg>
+  const [openModal, setOpenModal] = useState(false);
+  const el = document.getElementById('catalogue-search');
+  if (!el) return null;
+
+  if (!openModal) {
+    return ReactDOM.createPortal(
+      <a data-toggle="modal" data-backdrop="false" data-target="#searchModal" onClick={() => setOpenModal(true)}>
+        <div className="form-inline pr-2 pr-lg-0" role="search">
+          <div className="form-group position-relative">
+            <span className="text-white mr-3 d-none d-lg-inline">CERCA</span>
+            <svg className="icon">
+              <use xlinkHref="/assets/svg/sprite.svg#it-search"></use>
+            </svg>
+          </div>
+        </div>
+      </a>,
+      el
+    );
+  }
+
+  return ReactDOM.createPortal(
+    <div role="dialog" className="modal fade show d-block">
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <div className="container">
+              <p className="h1 modal-title d-inline"> Title </p>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+                onClick={() => setOpenModal(false)}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          </div>
+          <div className="modal-body">
+            <div className="container">
+              <div className="form-group">
+                <div className="autocomplete">
+                  <svg className="icon icon-lg" onClick={() => (location.pathname = 'it/search')}>
+                    <use xlinkHref="/assets/svg/sprite.svg#it-search"></use>
+                  </svg>
+                  <input type="text" className="form-control" placeholder="test" aria-label="test" />
+                  <div className="d-none"></div>
+                </div>
+                <div>
+                  <a className="btn py-2 px-4 mb-2 btn-filter all active" data="all">
+                    <svg className="icon">
+                      <use xlinkHref="/assets/svg/sprite.svg#it-search"></use>
+                    </svg>
+                    Tutto
+                  </a>
+                  <a className="btn py-2 px-4 mb-2 btn-filter platforms" data="platforms">
+                    <span className="icon icon-type-platform--gray"></span>piattaforme
+                  </a>
+                  <a className="btn py-2 px-4 mb-2 btn-filter software_open" data="software_open">
+                    <span className="icon icon-type-generic-sw--gray"></span>open software
+                  </a>
+                  <a className="btn py-2 px-4 mb-2 btn-filter reuse_software" data="reuse_software">
+                    <span className="icon icon-type-public-sw--gray"></span>riuso
+                  </a>
+                  <a className="btn py-2 px-4 mb-2 btn-filter administrations" data="administrations">
+                    <svg className="icon">
+                      <use xlinkHref="/assets/svg/sprite.svg#it-pa"></use>
+                    </svg>
+                    amministrazione
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </a>
+    </div>,
+    el
   );
 };
